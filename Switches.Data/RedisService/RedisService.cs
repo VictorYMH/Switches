a9884feccd8cedcb4c996f32a57fa5258c02a928
Victor Yu <ymhvic@outlook.com>
2020-05-18 16:47:10 +0800

    init commit

---
+++ Switches.Data/RedisService/RedisService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using StackExchange.Redis;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Switches.Data.RedisService
+{
+    public class RedisService
+    {
+        private readonly string _redisHost;
+        private readonly string _redisPassword;
+        private readonly int _redisPort;
+        private ConnectionMultiplexer _redis;
+
+        public RedisService(IConfiguration config)
+        {
+            _redisHost = config["Redis:Host"];
+            _redisPort = Convert.ToInt32(config["Redis:Port"]);
+            _redisPassword = config["Redis:Password"];
+        }
+
+        public void Connect()
+        {
+            //var configString = $"{_redisHost}:{_redisPort}";
+            var configString = $"{_redisHost}:{_redisPort},password={_redisPassword},connectRetry=5";
+            _redis = ConnectionMultiplexer.Connect(configString);
+        }
+
+        public async Task<bool> SetArray(string key, IEnumerable<object> values)
+        {
+            var db = _redis.GetDatabase(); 
+            return db.StringSet(key, JsonConvert.SerializeObject(values));
+        }
+
+        public async Task<RedisValue> Get(string key)
+        {
+            var db = _redis.GetDatabase();
+            return db.StringGet(key);
+        }
+
+    }
+}
---
+++ Switches/Controllers/SwitchController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Switches.Data.RedisService;
+
+namespace Switches.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SwitchController : ControllerBase
+    {
+
+        private readonly ILogger<SwitchController> _logger;
+        private readonly RedisService _redisService;
+
+        public SwitchController(ILogger<SwitchController> logger, RedisService redisService)
+        {
+            _logger = logger;
+            _redisService = redisService; 
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return await _redisService.Get("Switches");
+        }
+
+        [HttpPost]
+        public async Task<bool> POST(IEnumerable<object> statuses)
+        {
+            return await _redisService.SetArray("Switches", statuses);
+        }
+    }
+}
